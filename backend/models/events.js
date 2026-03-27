f2aa8da70375fad6c60da1e4c387733118aa8672
Muhammad Noor Ahmed <muhammadnoorahmed946@gmail.com>
2026-03-28 03:13:11 +0500

    implemented database models

---
+++ backend/models/events.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['Digital Art', 'Music', 'Technology', 'Networking']
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    imageUrl: {
+        type: String
+    },
+    organizer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
