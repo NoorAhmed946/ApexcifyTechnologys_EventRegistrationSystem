206e0861b65b7e9ff753f6ae56441e155567a622
Muhammad Noor Ahmed <muhammadnoorahmed946@gmail.com>
2026-03-29 18:21:40 +0500

    implemented registration controller

---
+++ backend/controllers/registrationController.js
@@ -0,0 +1,36 @@
+const Registration = require('../models/Registration');
+const Event = require('../models/Event');
+
+const registerForEvent = async (req, res) => {
+    const eventId = req.params.eventId;
+    const userId = req.user._id;
+
+    //  Check if event exists
+    const event = await Event.findById(eventId);
+    if (!event) {
+        res.status(404);
+        throw new Error('Event not found');
+    }
+
+    //  Check if already registered
+    const alreadyRegistered = await Registration.findOne({ user: userId, event: eventId });
+    if (alreadyRegistered) {
+        res.status(400);
+        throw new Error('You are already registered for this event');
+    }
+
+    // Create registration
+    const registration = await Registration.create({
+        user: userId,
+        event: eventId
+    });
+
+    res.status(201).json({ message: 'Successfully registered!', registration });
+};
+
+const getMyRegistrations = async (req, res) => {
+    const registrations = await Registration.find({ user: req.user._id }).populate('event');
+    res.json(registrations);
+};
+
+module.exports = { registerForEvent, getMyRegistrations };
