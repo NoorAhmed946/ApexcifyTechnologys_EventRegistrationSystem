eb8f257a4c2e70d2fad373c4bcf7cd5d829be485
Muhammad Noor Ahmed <muhammadnoorahmed946@gmail.com>
2026-03-29 18:28:24 +0500

    completed server commands

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 
-dotenv.config(); // loading variables
+const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const registrationRoutes = require('./routes/registrationRoutes');
+
+//  Error Middleware 
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+
+dotenv.config(); // Load variables
 connectDB();
 
 const app = express();
+app.use(express.json());
+
+// API Routes 
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/registrations', registrationRoutes);
+
+//Error Handling Middlewares
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});                                                                                                                                             
+});
